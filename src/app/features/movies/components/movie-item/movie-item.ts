e9d183013043d9bb88f307d1fe96dd3faaac4893
yarob-alrifai <yalrifai41@gmail.com>
2025-11-08 08:19:17 +0300

    feat: config component movie-item

--- src/app/features/movies/components/movie-item/movie-item.ts
+++ src/app/features/movies/components/movie-item/movie-item.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule, DecimalPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../../../core/models/movie.model';
 
 @Component({
   selector: 'app-movie-item',
-  imports: [],
+  imports: [CommonModule, DecimalPipe],
   templateUrl: './movie-item.html',
   styleUrl: './movie-item.scss',
 })
 export class MovieItem {
-
+  @Input({ required: true }) movie!: Movie;
 }
