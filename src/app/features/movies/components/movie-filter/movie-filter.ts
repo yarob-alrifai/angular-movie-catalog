23e83762e46869be7d892a51cf6ce6a8cfc9721b
yarob-alrifai <yalrifai41@gmail.com>
2025-11-08 08:21:48 +0300

    feat: config component movie-filter

--- src/app/features/movies/components/movie-filter/movie-filter.ts
+++ src/app/features/movies/components/movie-filter/movie-filter.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-filter',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './movie-filter.html',
   styleUrl: './movie-filter.scss',
 })
 export class MovieFilter {
+  private readonly querySignal = signal('');
 
+  @Output() readonly search = new EventEmitter<string>();
+
+  protected get query(): string {
+    return this.querySignal();
+  }
+
+  protected set query(value: string) {
+    this.querySignal.set(value);
+    this.search.emit(value);
+  }
+
+  protected clear(): void {
+    this.query = '';
+  }
 }
